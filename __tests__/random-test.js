0df7946b2f4b45036a63a6ed0f1f448a44026482
lykong <konglingy_lady@sina.com>
2014-12-29 10:43:14 +0800

    Delete anotation,change mock.

--- __tests__/random-test.js
+++ __tests__/random-test.js
@@ -1,1 +1,12 @@
 jest.dontMock("../js/model/random");
+
+describe("Random", function() {
+  describe("#getRandom", function() {
+    it("should return four of random number and dont same", function() {
+      var Random = require("../js/model/random");
+
+      var result = Random.getRandom();
+      expect(result).toBeTruthy();
+    });
+  });
+});
